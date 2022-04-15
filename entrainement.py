fb7db1fa7fc31759ea5875e7912591e4f441779a
Arovisann <alexandrerodriguezv@gmail.com>
2022-04-15 16:59:23 +0200

    first commit

---
+++ entrainement.py
@@ -0,0 +1,103 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import pathlib
+import random
+
+image_path = os.path.dirname(r'C:/Users/alexa/OneDrive/Bureau/travail/data/')
+data = pathlib.Path(image_path)
+
+image_count = len(list(data.glob('*/*.png')))
+print(image_count)
+
+
+batch_size = 32
+img_height = 28
+img_width = 28
+
+#préparation des données
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+  # vérification des labels
+
+class_names = train_ds.class_names
+print(class_names)
+
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
+
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+plt.show()
+
+print('hello world')
+
+num_classes = 4
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+
+
+history = model.fit(train_ds,validation_data=val_ds,epochs=30)
+
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['Train'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['Train'], loc='upper left')
+plt.show()
+
+#save model
+
